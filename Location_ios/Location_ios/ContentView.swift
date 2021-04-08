4e176d2e97a95d23e057f47c137fbab477c77653
d976045024 <976045024@qq.com>
2021-04-08 16:44:06 +0800

    update

--- Location_ios/Location_ios/ContentView.swift
+++ Location_ios/Location_ios/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isOn: Bool
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            isOn.toggle()
+        }) {
+            VStack {
+                Text(isOn ? "暂停" : "开始")
+                    .padding(3)
+                Image(systemName: isOn ? "pause.circle" : "play.circle")
+                    .frame(width: 20, height: 20)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    @State static var on: Bool = false
     static var previews: some View {
-        ContentView()
+        ContentView(isOn: false)
     }
 }
