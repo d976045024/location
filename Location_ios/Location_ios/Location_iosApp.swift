15d9d0383a4b3706c0a8bf437dacc4c7d0ec1faa
d976045024 <976045024@qq.com>
2021-04-08 15:19:55 +0800

    add directory

---
+++ Location_ios/Location_ios/Location_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Location_iosApp.swift
+//  Location_ios
+//
+//  Created by QHDi on 2021/4/8.
+//
+
+import SwiftUI
+
+@main
+struct Location_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
